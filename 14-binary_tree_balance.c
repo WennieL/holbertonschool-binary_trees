258b6be87ebcf1534790b9aa26b64d9c6ac64b12
WennieL <jecxu@hotmail.com>
2024-04-09 11:12:56 +0000

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,17 +12,15 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0, right_height = 0;
-	
+
 	if (tree == NULL)
 		return (0);
-	
-	if (tree->left != NULL)
-		left_height = 1 +  binary_tree_height(tree->left);
-	return (0);
 
-	if (tree->right != NULL)
-		right_height = 1 + binary_tree_height(tree->right);
-	return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	left_height = 1 +  binary_tree_height(tree->left);
+	right_height = 1 + binary_tree_height(tree->right);
 
 	return (left_height > right_height ? left_height : right_height);
 }
@@ -42,19 +40,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-	{
-		left = binary_tree_height(tree->left) + 1;
-	}
-	else
-		left = 0 - 1;
-
-	if (tree->right != NULL)
-	{
-		right = binary_tree_height(tree->right) + 1;
-	}
-	else
-		right = 0 - 1;
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
 	return (left - right);
 }
