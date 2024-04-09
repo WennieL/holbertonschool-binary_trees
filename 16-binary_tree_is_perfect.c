7708390a498f9c495868c2587e4c567634c745d6
WennieL <jecxu@hotmail.com>
2024-04-09 09:07:50 +0000

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,20 +11,21 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left, right;
-
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-
-	if (left != right)
-		return (0);
-
-	if (binary_tree_is_full(tree) && (left == right))
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	return (0);
+
+	if (binary_tree_height(tree->left) != binary_tree_height(tree->right))
+	{
+		return (0);
+	}
+	else
+	{
+		return (binary_tree_is_perfect(tree->left) &&
+			      (binary_tree_is_perfect(tree->right)));
+	}
 }
 
 
@@ -62,25 +63,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 		return (right_height);
-}
-
-/**
- * binary_tree_is_full - the function checks if a binary trees is full
- * @tree: a pointer to the root node of the tree to check
- *
- * Return: count of the full tree
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
