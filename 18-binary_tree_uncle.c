7d0cdeab0df60b1104d6bc3ab62e17c71ba17927
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-10 18:13:59 +1000

    include first method - 18-binary_tree_uncle.c
    Including the first working method for this question

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -30,3 +30,21 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (grandparent->left);
 	}
 }
+
+/* Original method */
+
+/**
+ * binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+ * {
+ *       if (node == NULL)
+ *             return (NULL);
+ *
+ *       if (node->parent == NULL || node->parent->parent == NULL)
+ *              return (NULL);
+ *
+ *       if (node->parent->parent->left == node->parent)
+ *       	 return (node->parent->parent->right);
+ *	 else 
+ *       	 return (node->parent->parent->left);
+ * }
+ */
